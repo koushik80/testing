43ba19a3f4dcfd9500979b6565bf86c2388ede59
koushik <koushik.dey@live.com>
2022-08-22 13:21:30 +0300

    test strings

--- tests/algebrs.test.js
+++ tests/algebrs.test.js
@@ -3,28 +3,29 @@ const { sum, mul, sub, div } = require('../algebra');
 describe('Algebra testing', () => {
     test('counting 1 + 1 = 2', () => {
         expect(sum(1, 1)).toBe(2);
-    })
+    });
 
     test('counting 1 * 1 = 1', () => {
         expect(mul(1, 1)).toBe(1);
-    })
+    });
 
     test('counting 1 - 1 =,', () => {
         expect(sub(1, 1)).toBe(0);
-    })
+    });
 
     test('counting 1/1 = 1', () => {
         expect(div(1, 1)).toBe(1);
-    })
+    });
 
     test('Adds 2 + 2 to NOT equal 5', () => {
         expect(sum(2, 2)).not.toBe(5);
-    })
+    });
 
     test('Test floating point numbers', () => {
         const results = sum(4.2, 3.2);
         expect(results).toBeGreaterThan(7.0);
-    })
+    });
+
 });
 
 
--- tests/util.test.js
+++ tests/util.test.js
@@ -1,7 +1,32 @@
 const { greet } = require ('../util');
+
 describe ('Greet Feature', () => {
     it ('Should get the greet message',() => {
         const results = greet ('Jane');
         expect(results).toBe('Hello Jane');
+    });
+});
+
+describe('.toMatchObject(object)', () => {
+    const houseForSale = {
+        bath: true,
+        bedrooms: 4,
+        kitchen: {
+            amenities: ['oven', 'stove', 'washer'],
+            area: 20,
+            wallColor: 'white',
+        },
+
+    };
+
+    const desiredHouse = {
+        bath: true,
+        kitchen: {
+            amenities: ['oven', 'stove', 'washer'],
+            wallColor: expect.stringMatching(/white|yellow/),
+        },
+    };
+    test('The house has my desired features', () => {
+        expect(houseForSale).toMatchObject(desiredHouse);
     });
 });
