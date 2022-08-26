998e11d93c32eab6ed2f240de95bca70c0e1528d
koushik <koushik.dey@live.com>
2022-08-26 11:34:43 +0300

    tiangle

---
+++ triangle.js
@@ -0,0 +1,17 @@
+function Triangle (side1, side2, side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+}
+Triangle.prototype.checkType = function() {
+    if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) ||    (this.side3 > (this.side1 + this.side2))) {
+      return "not a triangle";
+    } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) &&    ((this.side2 !== this.side3))) {
+      return "Epäsäännöllinen kolmio";
+    } else if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
+        return "Tasasivuinen kolmio";
+    } else {
+      return "Tasakylkinen kolmio";
+    }
+  }
+module.exports = { Triangle };
